d613ea2690ebb3587d00a7a7c88afad314eca18f
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2024-07-13 16:27:55 +0200

    perf: Add a limited number of attemps + error handling for  unexpected value

--- Random_number.java
+++ Random_number.java
@@ -1,24 +1,36 @@
 import java.util.Random;
-import java.util.Scanner;;
+import java.util.Scanner;
 
 public class Random_number {
     public static void main(String[] args) {
 
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+        Random random = new Random(); //Object for random number
+        Scanner scanner = new Scanner(System.in); // Obtain user's input
         int player_guess;
+        boolean win_status = false;
 
-        int random_number = random.nextInt(100) + 1;
+        int random_number = random.nextInt(100) + 1; //Generate random number
         System.out.println("Try to guess the mysterious number :");
-        player_guess = scanner.nextInt();
-
-        if(random_number > player_guess)
-            System.out.println("High");
-        else if (random_number < player_guess) {
-            System.out.println("Low");
-        } else {
-            System.out.println("Jackpot! You've found it");
+        try {
+            player_guess = scanner.nextInt();
+            for (int i = 0; i != 5; i++) {
+                if (random_number > player_guess) {
+                    System.out.println("Higher");
+                    player_guess = scanner.nextInt();
+                } else if (random_number < player_guess) {
+                    System.out.println("Lower");
+                    player_guess = scanner.nextInt();
+                } else {
+                    System.out.println("Jackpot! You've found it");
+                    win_status = true;
+                    break;
+                }
+            }
+            if (win_status == false)
+                System.out.println("Game Over. You have used up all your attempts");
+            scanner.close();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("I see you, but it doesn't work like that :)");
         }
-        scanner.close();
     }
 }
